6bece588f77abe751b2b084c9d379738b2fedb41
larissaoli <85039327+larissaoli@users.noreply.github.com>
2022-07-24 15:44:26 -0300

    teste

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,3 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
-streamlit uber_pickups.py
-
-streamlit https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/streamlit_app.py
